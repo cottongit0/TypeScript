a12bafc9beaa42416455258983d30bc22a8e585e
cottongit0 <cottongit0@gmail.com>
2021-05-03 23:38:38 +0900

    quiz 1 calc

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,24 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+
+function calculate(calc: string, num1: number, num2: number): number {
+  switch (calc) {
+    case "add":
+      return num1 + num2;
+    case "substract":
+      return num1 - num2;
+    case "multiply":
+      return num1 * num2;
+    case "divide":
+      return num1 / num2;
+    case "remainder":
+      return num1 % num2;
+  }
+}
+
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
