6eb8788c4e08587882ec7ada02b341a8acad8b20
cottongit0 <cottongit0@gmail.com>
2021-05-04 14:35:05 +0900

    quiz loading

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -3,24 +3,34 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  function printLoginState(login: ResourceLoadState) {
+    if (login.state === "success") {
+      console.log(`${login.response.body}`);
+    } else if (login.state === "loading") {
+      console.log(`${login.state}`);
+    } else {
+      console.log(`${login.reason}`);
+    }
+  }
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "😃 loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "😱 no network" }); // 😱 no network
 }
--- basic/1-6-discriminated.ts
+++ basic/1-6-discriminated.ts
@@ -11,20 +11,20 @@
   };
   type LoginState = SuccessState | FailState;
 
-  function login(id: string, password: string): LoginState {
-    return {
-      result: "success",
-      response: {
-        body: "로그인 성공",
-      },
-    };
-  }
+  // function login(id: string, password: string): LoginState {
+  //   return {
+  //     result: "success",
+  //     response: {
+  //       body: "로그인 성공",
+  //     },
+  //   };
+  // }
 
-  function printLoginState(state: LoginState) {
-    if (state.result === "success") {
-      console.log(`성공. ${state.response.body}`);
-    } else {
-      console.log(`실패. ${state.reason}`);
-    }
-  }
+  // function printLoginState(state: LoginState) {
+  //   if (state.result === "success") {
+  //     console.log(`성공. ${state.response.body}`);
+  //   } else {
+  //     console.log(`실패. ${state.reason}`);
+  //   }
+  // }
 }
