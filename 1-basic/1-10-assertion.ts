b90a75210880570c96e3a4b6ffcd8d5ff6b549a7
cottongit0 <cottongit0@gmail.com>
2021-05-04 18:05:23 +0900

    oop 개념

--- 1-basic/1-10-assertion.ts
+++ 1-basic/1-10-assertion.ts
@@ -9,4 +9,12 @@
 
   const wrong: any = 5;
   console.log((wrong as Array<number>).push(1));
+
+  function findNumbers(): number[] | undefined {
+    return undefined;
+  }
+  const numbers = findNumbers();
+  numbers!.push(2);
+
+  const button = document.querySelector("class")!;
 }
